3e070e1500833b2d66514424c71789bf78397e89
Oren Ben-Meir <orenbm11@gmail.com>
2020-03-29 19:13:32 -0400

    web scraped online pdfs

--- QChat/app/web_scrape/scrap.py
+++ QChat/app/web_scrape/scrap.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests 
 import json
+from scrap_pdf import convert_online_pdf_to_txt
 
 src = requests.get('https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports').text
 soup = BeautifulSoup(src, 'lxml')
@@ -10,25 +11,36 @@ Situation_Report = article.div.div
 
 res=[[],[]]
 
+who_data = []
+
 for report in Situation_Report.find_all('p'):
 	text = report.text
 	print(text)
-	res[0].append(text)
 
-	pdf = report.find('a')['href']
-	pdf = pdf.split('/')[5]
+	try:
+		pdf = report.find('a')['href']
+		pdf = pdf.split('/')[5]
+	except:
+		print('no more links, pdf loading done')
+		break
 	link =  f'https://www.who.int/docs/default-source/coronaviruse/situation-reports/{pdf}'
 	print(link)
-	res[1].append(link)
 
-	print()
+	res[0].append(text)
+	res[1].append(link)
+	who_data.append({
+		'text': convert_online_pdf_to_txt(link).replace("\n", " "),
+		'valid': 0
+	})
 
 	with open("report.json", "w") as write_file:
 		json.dump(res, write_file)
-	
+
+with open('who_pdfs.json', 'w') as write_file:
+	json.dump(who_data, write_file)
 
 
-# This script will scrap information off of the world health organization on Situation reports related to COVID-19
+# This script will scrap information off of tvhe world health organization on Situation reports related to COVID-19
 # and store the output as a JSON file 
 
 # Hopefully we can try to feed this data into the Neural Network!!! 
--- QChat/app/web_scrape/scrap_pdf.py
+++ QChat/app/web_scrape/scrap_pdf.py
@@ -26,3 +26,4 @@ if __name__ == '__main__':
     url = 'https://www.who.int/docs/default-source/coronaviruse/situation-reports/20200329-sitrep-69-covid-19.pdf?sfvrsn=8d6620fa_2'
     txt = convert_online_pdf_to_txt(url)
     print(txt)
+    print(type(txt))
