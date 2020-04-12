2c5c9f8e948ee3a03915a0ff7bcd1a48619fa176
Oren Ben-Meir <orenbm11@gmail.com>
2020-04-12 08:39:58 -0400

    disabled SQLALCHEMY_TRACK_MODIFICATIONS

--- app/config.py
+++ app/config.py
@@ -3,15 +3,16 @@ import os
 
 class Config:
     DOMAIN = "qchat.social"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False                    # prevents SQL alchemy FSADeprecationWarning warning
     FLASK_ENV = os.environ.get('FLASK_ENV')
     CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
     CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
     CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
-    SECRET_KEY = os.environ.get('SECRET_KEY')                               # Oren This will not work for you!
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')     # Oren This will not work for you!
+    SECRET_KEY = os.environ.get('SECRET_KEY')                               
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')     
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')                # Oren This will not work for you! Mine is stored in '.bash_profile'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')                # Oren This will not work for you! Mine is stored in '.bash_profile'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')                
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')           
     MONGO_URI=os.environ.get('MONGO_URI')
